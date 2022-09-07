7da19a57b9f530b6c4f01ee141af424cc9b8b101
fonkwe <williajeni@gmail.com>
2022-09-07 07:07:30 -0500

    completed routing and created paages with ejs

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path= require('path');
+const ejs = require('ejs');
+
 const app = express();
 
 dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 3000;
 //log request
 app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyParser.urlencoded({extended: true}))
+
+//set view engine
+app.set('view engine', 'ejs');
+// app.set('views', path.resolve(__dirname, 'views'))
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+
 app.get('/', (req, res) => {
-    res.send("User Management System");
+    res.render('index', {
+        title: "User Management System"
+    });
 })
 
 app.listen(PORT, () => {
